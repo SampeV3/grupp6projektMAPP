f4e6ca000e07c38470fe51485c3f12f4da4d4d18
noob code <herman.j@hotmail.se>
2024-04-12 19:19:19 +0200

    Improved enemy

--- Assets/EnemyAI.cs
+++ Assets/EnemyAI.cs
@@ -9,12 +9,15 @@ public class EnemyAI : MonoBehaviour
     [SerializeField] private GameObject projectilePrefab;
     //[SerializeField] private AudioClip projectileSFX;
     [SerializeField] private Transform player;
+    [SerializeField] private LayerMask playerMask, obstacleMask;
 
     private AudioSource audioSource;
     private Material originalMat;
     private SpriteRenderer sprd;
     private Animator anim;
     private int HP;
+    private bool checkLOS = false;
+    private Coroutine moveCoroutine, combatCoroutine;
 
     void Start()
     {
@@ -22,12 +25,68 @@ public class EnemyAI : MonoBehaviour
         audioSource = GetComponent<AudioSource>();
         anim = GetComponent<Animator>();
         //originalMat = sprd.material;
-        StartCoroutine(Combat());
+        
+        moveCoroutine = StartCoroutine(MoveAround());
+    }
+
+    private IEnumerator MoveAround()
+    {
+        while (true)
+        {
+            float distance = 0f;
+            Vector2 randomDirection = Vector2.zero;
+            while (distance < 4f)
+            {
+                randomDirection = Random.insideUnitCircle.normalized;
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, randomDirection, 50f,obstacleMask);
+                if (hit.collider != null && hit.collider.CompareTag("Wall") && hit.distance >= 4f)
+                {
+                    distance = hit.distance;
+                }
+            }
+            Vector2 targetPosition = new Vector2(transform.position.x, transform.position.y) + randomDirection * Random.Range(1f,distance-1.9f);
+            if (targetPosition.x > transform.position.x)
+            {
+                transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x),transform.localScale.y,transform.localScale.z);
+            }
+            else
+            {
+                transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x) * -1, transform.localScale.y, transform.localScale.z);
+            }
+            while (Vector2.Distance(transform.position, targetPosition) > 0.01f)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, targetPosition, 1.6f * Time.deltaTime);
+                yield return null;
+            }
+            yield return new WaitForSeconds(2f);
+        }
+    }
+
+    private void Update()
+    {
+        if (checkLOS)
+        {
+            CheckLineOfSight();
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //StartCoroutine(Combat());
+        if (collision.gameObject.CompareTag("Player"))
+        {
+        checkLOS = true;
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            checkLOS = false;
+            StopCoroutine(combatCoroutine);
+
+        }
     }
 
     private IEnumerator Combat()
@@ -53,11 +112,26 @@ public class EnemyAI : MonoBehaviour
     private void RangedAttack()
     {
         
-        GameObject projectile = Instantiate(projectilePrefab, new Vector2(transform.position.x, transform.position.y+ 1f), Quaternion.identity);
+        GameObject projectile = Instantiate(projectilePrefab, new Vector2(transform.position.x, transform.position.y), Quaternion.identity);
         Vector2 direction = (player.position - projectile.transform.position).normalized;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         projectile.GetComponent<Rigidbody2D>().rotation = angle;
         projectile.GetComponent<Rigidbody2D>().velocity = direction * 3.5f;
+    }
+
+    private void CheckLineOfSight()
+    {
+        Vector3 directionToPlayer = player.position - transform.position;
+
+        RaycastHit2D hitPlayer = Physics2D.Raycast(transform.position, directionToPlayer, 10f, playerMask);
+        RaycastHit2D hitWall = Physics2D.Raycast(transform.position, directionToPlayer, 10f, obstacleMask);
+
+        if (hitWall.collider.CompareTag("Wall") && hitPlayer.collider.CompareTag("Player") && hitPlayer.distance<hitWall.distance)
+        {
+            checkLOS = false;
+            StopCoroutine(moveCoroutine);
+            combatCoroutine = StartCoroutine(Combat());
+        }
     }
     private IEnumerator Flash()
     {
