c32ca81b12654e90896bb155915affb9a4b790c5
SampeV3 <112610662+SampeV3@users.noreply.github.com>
2024-04-20 17:05:48 +0200

    Added some stuff for the settings menu and fixed buttons in main menu further

--- Assets/Scripts/ButtonVibrationHandler.cs
+++ Assets/Scripts/ButtonVibrationHandler.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -31,6 +29,22 @@ public class ButtonVibrationHandler : MonoBehaviour
     {
         Debug.Log("Default Vibration performed!");
         Handheld.Vibrate();
+    }
+
+    private void LightVibration()
+    {
+        Debug.Log("Light Vibration performed!");
+    }
+
+    private void MediumVibration()
+    {
+        Debug.Log("Medium Vibration performed!");
+
+    }
+
+    private void HeavyVibration()
+    {
+        Debug.Log("Heavy Vibration performed!");
 
     }
 }
