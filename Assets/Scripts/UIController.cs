4acf7e922451b9eb84b9871bf216a21c51f2978b
basirghani <144438561+basirghani@users.noreply.github.com>
2024-04-20 15:43:11 +0200

    Update UIController.cs

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -18,7 +18,7 @@ public class UIController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        xPPoint.text = "" + 1;
+        xPPoint.text = "" + playerSupervisor.XP;
     }
     public void OpenInventory()
     {
