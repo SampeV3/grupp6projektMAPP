3ff2be240d2aa019fed58ff5bed286d32b96cf2d
basirghani <144438561+basirghani@users.noreply.github.com>
2024-05-15 16:27:31 +0200

    Updated Localization

--- Assets/Scripts/Basir/LocaleSelector.cs
+++ Assets/Scripts/Basir/LocaleSelector.cs
@@ -6,6 +6,12 @@ public class LocaleSelector : MonoBehaviour
 {
     private bool _active = false;
 
+    private void Start()
+    {
+        int ID = PlayerPrefs.GetInt("LocaleKey", 0);
+        ChangeLocale(ID);
+    }
+
     public void ChangeLocale(int localeID)
     {
         if (_active)
@@ -16,11 +22,12 @@ public class LocaleSelector : MonoBehaviour
         StartCoroutine(SetLocale(localeID));
     }
 
-    private IEnumerator SetLocale(int localeID)
+    private IEnumerator SetLocale(int _localeID)
     {
         _active = true;
         yield return LocalizationSettings.InitializationOperation;
-        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[localeID];
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[_localeID];
+        PlayerPrefs.SetInt("LocaleKey", _localeID);
         _active = false;
     }
 }
