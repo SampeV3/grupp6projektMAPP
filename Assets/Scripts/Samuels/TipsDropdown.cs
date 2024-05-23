0364c7fef51352be2140e27c6ee1942ffc431507
SampeV3 <112610662+SampeV3@users.noreply.github.com>
2024-05-23 16:21:36 +0200

    dropdown update

--- Assets/Scripts/Samuels/TipsDropdown.cs
+++ Assets/Scripts/Samuels/TipsDropdown.cs
@@ -6,35 +6,70 @@ using TMPro;
 public class TipsDropdown : MonoBehaviour
 {
 
-    [SerializeField] GameObject Walk;
-    [SerializeField] GameObject Shoot;
+    [SerializeField] GameObject WalkPanel;
+    [SerializeField] GameObject ShootPanel;
+    [SerializeField] GameObject DashPanel;
+    [SerializeField] GameObject HealingPanel;
+    [SerializeField] GameObject PowerUpPanel;
+    [SerializeField] GameObject UpgradesPanel;
 
-    
+
     public void HandleChoosenTips(int tipValue)
     {
         if (tipValue == 0)
         {
-            Walk.SetActive(true);
+            WalkPanel.SetActive(true);
+            ShootPanel.SetActive(false);
+            DashPanel.SetActive(false);
+            HealingPanel.SetActive(false);
+            PowerUpPanel.SetActive(false);
+            UpgradesPanel.SetActive(false);
+
         }
         if (tipValue == 1)
         {
-
+            WalkPanel.SetActive(false);
+            ShootPanel.SetActive(true);
+            DashPanel.SetActive(false);
+            HealingPanel.SetActive(false);
+            PowerUpPanel.SetActive(false);
+            UpgradesPanel.SetActive(false);
         }
         if (tipValue == 2)
         {
-
+            WalkPanel.SetActive(false);
+            ShootPanel.SetActive(false);
+            DashPanel.SetActive(true);
+            HealingPanel.SetActive(false);
+            PowerUpPanel.SetActive(false);
+            UpgradesPanel.SetActive(false);
         }
         if (tipValue == 3)
         {
-
+            WalkPanel.SetActive(false);
+            ShootPanel.SetActive(false);
+            DashPanel.SetActive(false);
+            HealingPanel.SetActive(true);
+            PowerUpPanel.SetActive(false);
+            UpgradesPanel.SetActive(false);
         }
         if (tipValue == 4)
         {
-
+            WalkPanel.SetActive(false);
+            ShootPanel.SetActive(false);
+            DashPanel.SetActive(false);
+            HealingPanel.SetActive(false);
+            PowerUpPanel.SetActive(true);
+            UpgradesPanel.SetActive(false);
         }
         if (tipValue == 5)
         {
-
+            WalkPanel.SetActive(false);
+            ShootPanel.SetActive(false);
+            DashPanel.SetActive(false);
+            HealingPanel.SetActive(false);
+            PowerUpPanel.SetActive(false);
+            UpgradesPanel.SetActive(true);
         }
     }
 }
